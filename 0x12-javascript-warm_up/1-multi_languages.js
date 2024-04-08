be930f60cc19d37e9f8a8eae945f135011067ec3
simonebute <bedrockresources21@gmail.com>
2024-04-08 07:36:15 -0700

    task1

--- 0x12-javascript-warm_up/1-multi_languages.js
+++ 0x12-javascript-warm_up/1-multi_languages.js
@@ -1,4 +1,7 @@
 #!/usr/bin/node
-console.log('C is fun');
-console.log('Python is cool');
-Console.log('Javascript isamazing');
+const myVar1 = 'C is fun';
+console.log(myVar);
+const myVar2 = 'Python is cool';
+console.log(myVar2);
+const myVar3 = 'Javascript is amazing';
+Console.log(myVar3);
